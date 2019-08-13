a689e3ca8ca99621ba7cf572089a749af6858cf3
AlunoSCGames <alunoscgames@gmail.com>
2019-08-13 11:12:21 -0300

    personagemMove

---
+++ ProgBasicaMat/Assets/scripts/Player.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    Rigidbody2D  RB2D;
+    bool podepular;
+
+
+    void Start()
+    {
+        RB2D = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        move();
+    }
+
+    public void move()
+    {
+        bool a = Input.GetKey("a");
+        bool d = Input.GetKey("d");
+
+        if (a)
+        {
+            RB2D.velocity = new Vector2(-5, 0);
+        }
+
+        if (d)
+        {
+            RB2D.velocity = new Vector2(+5, 0);
+        }
+        
+    }
+}
