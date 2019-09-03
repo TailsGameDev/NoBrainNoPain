16230af4e892c67258f2c2499d86ece7a4bd321d
Luis <alunoscgames@gmail.com>
2019-09-03 11:30:25 -0300

    animacoes

---
+++ ProgBasicaMat/Assets/scripts/Ataque.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ataque : MonoBehaviour
+{
+    public int Dano;
+    bool ataque;
+    Animator animator;
+    GameObject go;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //GameObject g = animator.gameObject;
+        //go.GetComponent<Animator>();
+        animator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey("e"));
+       
+    }
+}
---
+++ ProgBasicaMat/Assets/scripts/playerANIM.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Animator : MonoBehaviour
+{
+    Animator animator;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        animator.SetFloat("HorizontalAxis",Input.GetAxis("Horizontal"));
+    }
+}
