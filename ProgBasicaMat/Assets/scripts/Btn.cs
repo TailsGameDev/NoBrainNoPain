972d6184b8aac1049be7f67a1295bc671e294a4c
Caina C. Caldas <cainacc@hotmail.com>
2019-09-10 15:10:49 -0300

    botao na cena de checkpoints

---
+++ ProgBasicaMat/Assets/scripts/Btn.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Btn : MonoBehaviour
+{
+    public void RecarregaCena () {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+}
