9de589589b56b7f3c1fa3eb58c822325b11db5b2
Luis <alunoscgames@gmail.com>
2019-10-22 11:27:53 -0300

    Continuação da bara de vomito

---
+++ ProgBasicaMat/Assets/scripts/BarraDeVomito.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BarraDeVomito : MonoBehaviour
+{
+    GameObject = barraVomito;
+    public float BarraDoPersonagem;
+    public RawImage barraVomito;
+    public float BaraCheia = 5;
+    float larguraBarraOriginal;
+    void Start()
+    {
+        
+        BarraDoPersonagem = BaraCheia;
+        larguraBarraOriginal = barraVomito.GetComponent<RawImage>().sizeDelta.x;
+
+
+    }
+    void Update()
+    {
+
+        if (BarraDoPersonagem >= BaraCheia)
+        {
+            BarraDoPersonagem = BaraCheia;
+        }
+        else if (BarraDoPersonagem <= 0)
+        {
+            BarraDoPersonagem = 0;
+        }
+        //aqui
+        barraVomito.GetComponent<rawimage>().sizeDelta =
+        new Vector2(BarraDoPersonagem * larguraBarraOriginal / BaraCheia, barraVomito.GetComponent<Rawimage>().sizeDelta.x);
+    }
+}
