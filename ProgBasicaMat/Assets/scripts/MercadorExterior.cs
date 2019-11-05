a9f428db85bbf8c6b2f2e45ada696a94ee04182a
Caina C. Caldas <cainacc@hotmail.com>
2019-11-05 12:08:17 -0300

    executor basico e mercador tambeem

---
+++ ProgBasicaMat/Assets/scripts/MercadorExterior.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MercadorExterior : MonoBehaviour
+{
+    public GameObject mercadorInterior;
+
+    private void OnTriggerEnter2D (Collider2D collision) {
+        mercadorInterior.SetActive(true);
+    }
+
+    private void OnTriggerExit2D (Collider2D collision) {
+        mercadorInterior.SetActive(false);
+    }
+}
