8560445b7b698f371894837af0d72af0d93e8281
aluno <alunoscgames@gmail.com>
2019-11-19 12:23:49 -0200

    compravel peh

---
+++ ProgBasicaMat/Assets/scripts/CompravelPe.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CompravelPe : Compravel
+{
+
+    public int qtdLimite;
+
+    private void Start()
+    {
+        if (Comprou.pes >= qtdLimite)
+        {
+            gameObject.SetActive(false);
+        }
+    }
+
+
+    protected override void Efeito()
+    {
+        Comprou.pes = Comprou.pes + 1;
+        GameObject player = GameObject.FindGameObjectWithTag("Player");
+        Player playerPlayer = player.GetComponent<Player>();
+        int pesQtdd = Comprou.pes;
+        while (pesQtdd > 0)
+        {
+            playerPlayer.speed += 1;
+            pesQtdd -= 1;
+        }
+    }
+}
+
