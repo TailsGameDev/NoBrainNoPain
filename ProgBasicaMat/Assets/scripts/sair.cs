59fe740d1d348481cdd8780c58761fdc27cb6e36
aluno <alunoscgames@gmail.com>
2019-11-26 11:58:00 -0300

    menu funcionando

---
+++ ProgBasicaMat/Assets/scripts/sair.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class sair : MonoBehaviour
+{
+    public void sairu() 
+    {
+      Application.Quit();
+    }
+}
