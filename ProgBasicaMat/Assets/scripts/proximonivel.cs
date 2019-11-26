392ee7625063d0fbf6e855cec0f19d0a7e2a4c4d
andre <andre>
2019-11-26 11:28:45 -0300

    porta

---
+++ ProgBasicaMat/Assets/scripts/proximonivel.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class proximonivel : MonoBehaviour
+{
+    public string proximaCena;
+
+    private void OnTriggerEnter2D(Collider2D col)
+    {
+        if(col.tag =="Player")
+        SceneManager.LoadScene(proximaCena);
+    }
+}
