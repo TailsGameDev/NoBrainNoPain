94b86560b0d07205609c85fbd427f4e99bd8ae41
Luis <alunoscgames@gmail.com>
2019-11-26 08:58:12 -0300

    tentando restaurar

--- ProgBasicaMat/Assets/scripts/Compravel.cs
+++ ProgBasicaMat/Assets/scripts/Compravel.cs
@@ -1,10 +1,13 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 //eh abstract para que seja criada uma nova classe para cada item a ser comprado
 public abstract class Compravel : MonoBehaviour
 {
+    [SerializeField] Text precoText;
+
     public void OnComprado (int preco) {
         // deu eh true se o player tinha cerebros suficientes
         bool deu = ColetaCerebros.coletor.Pagar(preco);
@@ -12,9 +15,34 @@ public abstract class Compravel : MonoBehaviour
         if (deu) {
             Efeito();
             gameObject.SetActive(false);
+        } else
+        {
+            StartCoroutine(PiscaTexto(precoText));
         }
     }
 
     protected abstract void Efeito();
+
+    IEnumerator PiscaTexto(Text text){
+        float tempoPiscando = 1f;
+        float t = tempoPiscando;
+        Color original = text.color;
+        text.color = Color.red;
+        while(t > 0.75*tempoPiscando){
+            t-= Time.deltaTime;
+            yield return null;
+        }
+        text.color = original;
+        while(t > 0.5*tempoPiscando){
+            t-= Time.deltaTime;
+            yield return null;
+        }
+        text.color = Color.red;
+        while(t > 0.25*tempoPiscando){
+            t-= Time.deltaTime;
+            yield return null;
+        }
+        text.color = original;
+    }
     
 }
--- ProgBasicaMat/Assets/scripts/DanoMili.cs
+++ ProgBasicaMat/Assets/scripts/DanoMili.cs
@@ -4,25 +4,30 @@ using UnityEngine;
 
 public class DanoMili : dadano
 {
-    float addvomito = 20f;
+    float addvomito = 20f; // soh eh usado se for uma mordida
     Ataque atk;
 
     private void Awake()
     {
         GameObject player = GameObject.FindGameObjectWithTag("Player");
-        atk = player.GetComponent<Ataque>();
+        if (tag == "mordida")
+        {
+            atk = player.GetComponent<Ataque>();
+        }
     }
 
     protected override void OnTriggerEnter2D(Collider2D col)
     {
-        base.OnTriggerEnter2D(col);
-        Invoke("Destroise", 0.0f);
+        base.OnTriggerEnter2D(col); // a logica de dano estah na superclasse
+        Invoke("Destroise", 0.2f);
 
-        if (col.tag=="Inimigo")
-        {
-            if (atk.getVomitoAtual() < 99)
+        if (tag == "mordida"){
+            if (col.tag == "Inimigo")
             {
-                atk.setVomitoAtual(atk.getVomitoAtual() + 20);
+                if (atk.getVomitoAtual() < 99)
+                {
+                    atk.setVomitoAtual(atk.getVomitoAtual() + 20);
+                }
             }
         }
            
