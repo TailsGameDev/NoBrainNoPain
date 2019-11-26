33dbc487a8b62ba82ef1030840245012e9cd4ba2
Luis <alunoscgames@gmail.com>
2019-11-26 08:49:57 -0300

    esetomago

---
+++ ProgBasicaMat/Assets/scripts/CompravelEstomago.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CompravelEstomago : Compravel
+{
+    public int qtdLimite;
+
+    private void Start()
+    {
+        if (Comprou.estamagos >= qtdLimite)
+        {
+            gameObject.SetActive(false);
+        }
+    }
+
+    protected override void Efeito()
+    {
+        Comprou.estamagos = Comprou.estamagos + 1;
+        GameObject player = GameObject.FindGameObjectWithTag("Player");
+        Ataque ataque = player.GetComponent<Ataque>();
+    }
+
+
+}
