f5aad36a7bae26aca6aa18f0fa13a4e2567bf1a1
Caina C. Caldas <cainacc@hotmail.com>
2019-12-03 12:56:22 -0300

    jogo publicado

--- ProgBasicaMat/Assets/scripts/CutScene.cs
+++ ProgBasicaMat/Assets/scripts/CutScene.cs
@@ -7,12 +7,16 @@ public class CutScene : MonoBehaviour
     Animator animator;
     [SerializeField] Menu menu;
     [SerializeField] string nextScene;
+    bool tahCarregando = false;
     void Start(){
         animator = GetComponent<Animator>();
     }
     void Update()
     {
+        if (tahCarregando) return;
+
         if (animator.GetCurrentAnimatorStateInfo(0).IsName("Acabou")){
+            tahCarregando = true;
             menu.GoToScene(nextScene);
         }
     }
